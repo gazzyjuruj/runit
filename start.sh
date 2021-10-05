9c310d214c82e024c3f3909225eb145ca4b01036
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-10-06 01:21:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.0/SRBMiner-Multi-0-8-0-Linux.tar.xz
-tar xf SRBMiner-Multi-0-8-0-Linux.tar.xz
-cd SRBMiner-Multi-0-8-0
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+cd cpuminer-opt-linux
 while [ 1 ]; do
-      ./guided-setup.sh
-      ./start_dgb.sh
+      ./cpuminer-avx2 -a scryptn2 -o stratum+tcp://scryptn2.na.mine.zpool.ca:4339 -u dgb1qcgzmf0qx5842xmd09nrc9u2xp76qty5fefuexd -p c=DGB
       sleep 0
 done
