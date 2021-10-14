b8d73ef68c836db0d401db213f4cb7fddb72c4f6
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-10-14 13:33:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ tar xf cpuminer-opt-linux.tar.gz
 cd cpuminer-opt-linux
 while [ 1 ]; do
       ./cpuminer-avx2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u 4yXfqzihbAnVywYS9A2hEM9WaXWeJr1cpq -p c=XMY
-      sleep 0
+      sleep 3
 done
